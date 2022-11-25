ae237762c81085a50478cf0d077f804311edfc9d
Mat-tapella <matheo.tapella@gmail.com>
2022-11-25 11:50:14 +0100

    Wip firebase
    configuration firebase web/android

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,23 @@
-import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 
-Future <void> main() async {
+Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+        options: const FirebaseOptions(
+            apiKey: "AIzaSyDZg9vr_ESsC9bnOzg_upD78A8yu-EPWHw",
+            authDomain: "atelier-dev-mobile.firebaseapp.com",
+            appId: "1:762403803198:web:5c5614ad460771cd13c16f",
+            messagingSenderId: "762403803198",
+            storageBucket: "atelier-dev-mobile.appspot.com",
+            projectId: "atelier-dev-mobile"));
+  } else {
+    await Firebase.initializeApp();
+  }
+
   runApp(const MyApp());
 }
 
